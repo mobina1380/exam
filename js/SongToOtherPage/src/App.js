860c61092be8d168444c049049438f4d5e9badbb
mobina <esmaeilimobina98@gmail.com>
2023-03-29 21:35:15 +0330

    slove some problem

---
+++ js/SongToOtherPage/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
+import Zarebin from "./features/search/zarebin";
+import Searchbar from "./features/search/zarebin";
+import Page from "./features/page/page";
+import PageItem from "./features/page/pageItem";
+import { useParams } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Searchbar />} />
+          <Route path="/search" element={<Page />}></Route>
+          <Route path="/search/:query" element={<Page />} />
+          <Route path="/search/:query/:type" element={<Page />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
